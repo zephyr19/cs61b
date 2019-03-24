c75766925f7b1d962ff30c3571ceba823ed3d24a
zephyr <550196504@qq.com>
2019-03-24 12:43:06 +0800

    proj1b finished

--- proj1b/OffByN.java
+++ proj1b/OffByN.java
@@ -1,8 +1,10 @@
 public class OffByN implements CharacterComparator {
     private int number;
+
     OffByN(int N) {
         number = N;
     }
+
     @Override
     public boolean equalChars(char x, char y) {
         if (Math.abs(x - y) == number) {

6191543620a85869167999436ccb43b9b31937d2
zephyr <550196504@qq.com>
2019-03-28 13:21:18 +0800

    lab6 finished

--- lab6/UnionFind.java
+++ lab6/UnionFind.java
@@ -20,6 +20,7 @@ public class UnionFind {
 
     /* Returns the size of the set v1 belongs to. */
     public int sizeOf(int v1) {
+        validate(v1);
         int root = parent(v1);
         while (root >= 0) {
             root = parent(root);
@@ -30,11 +31,13 @@ public class UnionFind {
     /* Returns the parent of v1. If v1 is the root of a tree, returns the
        negative size of the tree for which v1 is the root. */
     public int parent(int v1) {
+        validate(v1);
         return array[v1];
     }
 
     /* Returns true if nodes v1 and v2 are connected. */
     public boolean connected(int v1, int v2) {
+        validate(v1);
         return find(v1) == find(v2);
     }
 
@@ -44,6 +47,8 @@ public class UnionFind {
        vertex with itself or vertices that are already connected should not 
        change the sets but may alter the internal structure of the data. */
     public void union(int v1, int v2) {
+        validate(v1);
+        validate(v2);
         if (sizeOf(v1) <= sizeOf(v2)) {
             array[find(v2)] += array[find(v1)];
             array[find(v1)] = find(v2);
@@ -56,6 +61,7 @@ public class UnionFind {
     /* Returns the root of the set V belongs to. Path-compression is employed
        allowing for fast search-time. */
     public int find(int v1) {
+        validate(v1);
         int n = parent(v1), root = v1;
         while (n >= 0) {
             root = n;

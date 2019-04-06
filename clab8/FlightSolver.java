626b8c9d465feb70ed1698b20c98d0cb903cb7af
zephyr <550196504@qq.com>
2019-04-06 11:50:38 +0800

    clab8 first commit

--- clab8/FlightSolver.java
+++ clab8/FlightSolver.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 /**
  * Solver for the Flight problem (#9) from CS 61B Spring 2018 Midterm 2.
@@ -6,15 +8,30 @@ import java.util.ArrayList;
  * If a flight starts at the same time as a flight's end time, they are
  * considered to be in the air at the same time.
  */
-public class FlightSolver {
+public class FlightSolver extends PriorityQueue {
+
+    private PriorityQueue<Flight> pq;
 
     public FlightSolver(ArrayList<Flight> flights) {
-        /* FIX ME */
+        pq = new PriorityQueue<>();
+        pq.addAll(flights);
     }
 
     public int solve() {
-        /* FIX ME */
-        return -1;
+        Flight res = pq.peek();
+        if (res == null) {
+            throw new IllegalCallerException();
+        }
+        return res.passengers();
     }
 
+    @Override
+    public Comparator<Flight> comparator() {
+        return new Comparator<Flight>() {
+            @Override
+            public int compare(Flight o1, Flight o2) {
+                return o1.passengers() - o2.passengers();
+            }
+        };
+    }
 }

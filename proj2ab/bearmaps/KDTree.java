3ddf8b0d382e65c46a86814ebc04cd23f12da645
zephyr <550196504@qq.com>
2019-04-29 16:50:54 +0800

    proj2b finished.

--- proj2ab/bearmaps/KDTree.java
+++ proj2ab/bearmaps/KDTree.java
@@ -3,14 +3,103 @@ package bearmaps;
 import java.util.List;
 
 public class KDTree implements PointSet {
+    private class Node {
+        boolean ifLeft;
+        double x;
+        double y;
+        Node left;
+        Node right;
 
+        private Node(double x, double y, boolean b) {
+            this.x = x;
+            this.y = y;
+            this.ifLeft = b;
+        }
+    }
+
+    private Node root;
+    private Node best;
+    private double distance;
+
+    private void add(Point point) {
+        root = add(root, point, true); // First add left or right, then add top or down.
+    }
+
+    private Node add(Node node, Point point, boolean ifLeft) {
+        if (node == null) {
+            return new Node(point.getX(), point.getY(), ifLeft);
+        }
+        if (node.ifLeft) {
+            if (point.getX() < node.x) {
+                node.left = add(node.left, point, false);
+            } else {
+                node.right= add(node.right, point, false);
+            }
+        } else {
+            if (point.getY() < node.y) {
+                node.left = add(node.left, point,true);
+            } else {
+                node.right = add(node.right, point, true);
+            }
+        }
+        return node;
+    }
 
     public KDTree(List<Point> points) {
-
+        for (Point point : points) {
+            add(point);
+        }
     }
 
     @Override
     public Point nearest(double x, double y) {
-        return null;
+        distance = 10e5;
+        nearest(x, y,root);
+        if (best == null) {
+            throw new IllegalCallerException();
+        }
+        return new Point(best.x, best.y);
+    }
+
+    private void nearest(double x, double y, Node node) {
+        if (node == null) {
+            return;
+        }
+        double xDist = node.x - x;
+        double yDist = node.y - y;
+        double dist = xDist * xDist + yDist * yDist;
+        if (dist < distance) {
+            distance = dist;
+            best = node;
+        }
+        Node goodSide;
+        Node badSide;
+        if (node.ifLeft) {
+            if (x - node.x < 0) {
+                goodSide = node.left;
+                badSide = node.right;
+            } else {
+                goodSide = node.right;
+                badSide = node.left;
+            }
+        } else {
+            if (y - node.y < 0) {
+                goodSide = node.left;
+                badSide = node.right;
+            } else {
+                goodSide = node.right;
+                badSide = node.left;
+            }
+        }
+        nearest(x, y, goodSide);
+        if (node.ifLeft) {
+            if (Math.abs(x - node.x) < distance) {
+                nearest(x, y, badSide);
+            }
+        } else {
+            if (Math.abs(y - node.y) < distance) {
+                nearest(x, y, badSide);
+            }
+        }
     }
 }
--- proj2ab/bearmaps/KDTreeTest.java
+++ proj2ab/bearmaps/KDTreeTest.java
@@ -1,7 +1,82 @@
 package bearmaps;
 
+import edu.princeton.cs.algs4.StdRandom;
 import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
 import static org.junit.Assert.assertEquals;
 
 public class KDTreeTest {
+    @Test
+    /* It seems worked! */
+    public void KDTreeConstructor() {
+        Point p1 = new Point(1.1, 2.2); // constructs a Point with x = 1.1, y = 2.2
+        Point p2 = new Point(3.3, 4.4);
+        Point p3 = new Point(-2.9, 4.2);
+        Point p4 = new Point(1.1, 2.3);
+        List<Point> points = new LinkedList<>();
+        points.add(p1);
+        points.add(p2);
+        points.add(p3);
+        points.add(p4);
+        KDTree kdTree = new KDTree(points);
+    }
+
+    @Test
+    public void nearestSimpleTest() {
+        Point p1 = new Point(1.1, 2.2); // constructs a Point with x = 1.1, y = 2.2
+        Point p2 = new Point(3.3, 4.4);
+        Point p3 = new Point(-2.9, 4.2);
+        Point p4 = new Point(1.1, 2.3);
+        List<Point> points = new LinkedList<>();
+        points.add(p1);
+        points.add(p2);
+        points.add(p3);
+        points.add(p4);
+        KDTree kdTree = new KDTree(points);
+        assertEquals(p1, kdTree.nearest(1, 2));
+        assertEquals(p2, kdTree.nearest(3, 4));
+        assertEquals(p3, kdTree.nearest(-2, 4.22));
+        assertEquals(p4, kdTree.nearest(1.1, 2.4));
+    }
+
+    @Test
+    /* Test passed. Using 4 m 27 s 268ms. */
+    public void nearestRandomTest() {
+        List<Point> points = new LinkedList<>();
+        for (int i = 0; i < 1000; i++) {
+            double x = StdRandom.uniform(0, 10e3);
+            double y = StdRandom.uniform(0, 10e3);
+            Point point = new Point(x, y);
+            points.add(point);
+        }
+        KDTree kdTree = new KDTree(points);
+        NaivePointSet naivePointSet = new NaivePointSet(points);
+        for (int i = 0; i < 10e5; i++) {
+            double x = StdRandom.uniform(0, 10e3);
+            double y = StdRandom.uniform(0, 10e3);
+            assertEquals(kdTree.nearest(x, y), naivePointSet.nearest(x, y));
+        }
+    }
+
+    @Test
+    /* Using 7 s 805 ms. */
+    public void nearestRandomTimeTest() {
+        List<Point> points = new LinkedList<>();
+        for (int i = 0; i < 1000; i++) {
+            double x = StdRandom.uniform(0, 10e3);
+            double y = StdRandom.uniform(0, 10e3);
+            Point point = new Point(x, y);
+            points.add(point);
+        }
+        KDTree kdTree = new KDTree(points);
+        for (int i = 0; i < 10e5; i++) {
+            double x = StdRandom.uniform(0, 10e3);
+            double y = StdRandom.uniform(0, 10e3);
+            kdTree.nearest(x, y);
+        }
+    }
 }

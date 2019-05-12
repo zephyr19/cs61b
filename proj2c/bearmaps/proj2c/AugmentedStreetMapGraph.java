0080fb8237fe572440dfd855891e463019e7cbd5
zephyr <550196504@qq.com>
2019-05-12 09:24:00 +0800

    proj2c gold points finished

--- proj2c/bearmaps/proj2c/AugmentedStreetMapGraph.java
+++ proj2c/bearmaps/proj2c/AugmentedStreetMapGraph.java
@@ -18,22 +18,30 @@ import java.util.*;
  */
 public class AugmentedStreetMapGraph extends StreetMapGraph {
 
-    List<Point> points;
     private WeirdPointSet pointSet;
     private Map<Point, Long> pointLongMap;
     private MyTrieSet trieSet;
+    private Map<String, List<Node>> stringToNodes;
 
     public AugmentedStreetMapGraph(String dbPath) {
         super(dbPath);
         List<Node> nodes = this.getNodes();
         pointLongMap = new HashMap<>();
-        points = new LinkedList<>();
+        List<Point> points = new LinkedList<>();
         trieSet = new MyTrieSet();
+        stringToNodes = new HashMap<>();
         for (Node node : nodes) {
-            if (neighbors(node.id()).size() > 0) {
-                if (node.name() != null) {
-                    trieSet.add(node.name());
+            if (node.name() != null) {
+                String cleanName = cleanString(node.name());
+                trieSet.add(cleanName);
+                if (stringToNodes.containsKey(cleanName)) {
+                    stringToNodes.get(cleanName).add(node);
+                } else {
+                    stringToNodes.put(cleanName, new LinkedList<>());
+                    stringToNodes.get(cleanName).add(node);
                 }
+            }
+            if (neighbors(node.id()).size() > 0) {
                 Point point = new Point(node.lon(), node.lat());
                 points.add(point);
                 pointLongMap.put(point, node.id());
@@ -41,7 +49,6 @@ public class AugmentedStreetMapGraph extends StreetMapGraph {
         }
         pointSet = new WeirdPointSet(points);
     }
-
 
     /**
      * For Project Part II
@@ -65,7 +72,15 @@ public class AugmentedStreetMapGraph extends StreetMapGraph {
      * cleaned <code>prefix</code>.
      */
     public List<String> getLocationsByPrefix(String prefix) {
-        return trieSet.keysWithPrefix(prefix);
+        List<String> cleanName =  trieSet.keysWithPrefix(cleanString(prefix));
+        List<String> res = new LinkedList<>();
+        for (String name : cleanName) {
+            List<Node> list = stringToNodes.get(name);
+            for (Node node : list) {
+                res.add(node.name());
+            }
+        }
+        return res;
     }
 
     /**
@@ -82,7 +97,18 @@ public class AugmentedStreetMapGraph extends StreetMapGraph {
      * "id" -> Number, The id of the node. <br>
      */
     public List<Map<String, Object>> getLocations(String locationName) {
-        return new LinkedList<>();
+        List<Map<String, Object>> res = new LinkedList<>();
+        String cleanName = cleanString(locationName);
+        List<Node> nodes = stringToNodes.get(cleanName);
+        for (Node node : nodes) {
+            Map<String, Object> map = new HashMap<>();
+            map.put("lat", node.lat());
+            map.put("lon", node.lon());
+            map.put("name", node.name());
+            map.put("id", node.id());
+            res.add(map);
+        }
+        return res;
     }
 
 
